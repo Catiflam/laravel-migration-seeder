4f2dd695d6f5c7fb93d60505fd144cc1ee30e82a
Catiflam <gilfrid1@gmail.com>
2023-10-16 21:53:11 +0200

    edit migrate

---
+++ database/migrations/2023_10_16_193634_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->date_time('Orario di partenza'); 
+            $table->date_time('Orario di arrivo');
+            $table->tinyInteger('Codice Treno');
+            $table->tinyInteger('Numero Carrozze');
+            $table->date_time('orario');
+            $table->string('Cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
